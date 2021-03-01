46094cc930ba966fbac25133cac43754e785ff1c
Phage Ky <kyp0717@gmail.com>
2021-02-28 19:16:53 -0500

    add more features

--- emacs/init.el
+++ emacs/init.el
@@ -1,18 +1,15 @@
 (require 'package)
-(setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(package-initialize)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
+;; Bootstrap `use-package'
+(unless (package-installed-p 'use-package)  ; Unless it is already installed
+  (package-refresh-contents)                ; Update packages archive
+  (package-install 'use-package))           ; Install latest use-package
+(require 'use-package)
 
-(eval-when-compile
-  (require 'use-package))
-
-(org-babel-load-file (expand-file-name "~/workspace/emacs/settings.org"))
+(org-babel-load-file (expand-file-name "~/.emacs.d/settings.org"))
 ;; Enable company in all buffers
 ;; (add-hook 'after-init-hook 'global-company-mode)
 
@@ -30,9 +27,8 @@
  '(custom-enabled-themes '(gruvbox-dark-medium))
  '(custom-safe-themes
    '("7661b762556018a44a29477b84757994d8386d6edee909409fabe0631952dad9" "8d5f22f7dfd3b2e4fc2f2da46ee71065a9474d0ac726b98f647bc3c7e39f2819" default))
- '(helm-minibuffer-history-key "M-p")
  '(package-selected-packages
-   '(auto-complete merlin smartparens gruvbox-theme windresize evil ido org powerline smart-mode-line swiper ivy use-package tuareg smart-mode-line-powerline-theme projectile org-superstar org-bullets helm gruvbox-theme-dark fzf evil-org evil-commentary evil-collection counsel company command-log-mode beacon all-the-icons airline-themes)))
+   '(windresize eval-in-repl auto-complete merlin smartparens smartparen gruvbox-theme evil ido org powerline smart-mode-line swiper ivy use-package tuareg smart-mode-line-powerline-theme projectile org-superstar org-bullets helm gruvbox-theme-dark fzf evil-org evil-commentary evil-collection counsel company command-log-mode beacon all-the-icons airline-themes)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
